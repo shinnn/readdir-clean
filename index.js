ffd28d9228dbb0999aff4a1e67b1477979c2a37a
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-09-17 19:40:57 +0900

    use async/await
    [BREAKING] drop support for Node.js < 7

--- index.js
+++ index.js
@@ -6,33 +6,35 @@ const {readdir} = require('graceful-fs');
 
 const PATH_ERROR = 'Expected a directory path';
 
-module.exports = function readdirClean(...args) {
-  return new Promise((resolve, reject) => {
-    const argLen = args.length;
+module.exports = async function readdirClean(...args) {
+  const argLen = args.length;
 
-    if (argLen !== 1) {
-      throw new RangeError(`Expected 1 argument (string), but got ${
-        argLen === 0 ? 'no' : argLen
-      } arguments instead.`);
-    }
+  if (argLen !== 1) {
+    throw new RangeError(`Expected 1 argument (string), but got ${
+      argLen === 0 ? 'no' : argLen
+    } arguments instead.`);
+  }
 
-    const [dir] = args;
+  const [dir] = args;
 
-    if (typeof dir !== 'string') {
-      throw new TypeError(`${PATH_ERROR} (string), but got ${inspectWithKind(dir)}.`);
-    }
+  if (typeof dir !== 'string') {
+    throw new TypeError(`${PATH_ERROR} (string), but got ${inspectWithKind(dir)}.`);
+  }
 
-    if (dir.length === 0) {
-      throw new TypeError(`${PATH_ERROR}, but got '' (empty string).`);
-    }
+  if (dir.length === 0) {
+    throw new TypeError(`${PATH_ERROR}, but got '' (empty string).`);
+  }
 
+  const results = await new Promise((resolve, reject) => {
     readdir(dir, (err, paths) => {
       if (err) {
         reject(err);
         return;
       }
 
-      resolve(paths.filter(isActualContent));
+      resolve(paths);
     });
   });
+
+  return results.filter(isActualContent);
 };
